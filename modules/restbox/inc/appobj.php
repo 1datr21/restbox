f817729440062eac9dc361cc14c254934119457c
1datr21 <1datr@mail.ru>
2019-12-08 20:30:39 +0300

    --- Auto-Git Commit ---

---
+++ modules/restbox/inc/appobj.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace modules\restbox {
+	use Core;
+
+    class AppObject
+    {
+        function __construct()
+        {
+            
+        }
+
+        static function getKey()
+        {
+            
+        }
+    }
+
+}
