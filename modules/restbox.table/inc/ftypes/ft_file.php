0f50cda733710ebe82a8e30c0cfc2b33315fc018
1datr21 <1datr@mail.ru>
2019-12-21 11:37:25 +0300

    начали филе

--- modules/restbox.table/inc/ftypes/ft_file.php
+++ modules/restbox.table/inc/ftypes/ft_file.php
@@ -1,1 +1,16 @@
-<?php
+<?php 
+namespace modules\restbox\table {
+
+    class ft_file extends fieldttype_base
+    {
+        function OnCreate_std($_args)
+        {
+            return [
+                'fld_seg'=>"`{$this->fldname}` text NOT NULL ",
+                'add_queries'=>[]
+            ];
+        }
+    }
+
+}
+?>
