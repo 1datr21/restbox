f352efbc4b42ae5c2552bd0f5a118a479ccfd835
1datr21 <1datr@mail.ru>
2020-01-06 21:28:57 +0300

    --- Auto-Git Commit ---

--- modules/restbox.session/inc/authtable.php
+++ modules/restbox.session/inc/authtable.php
@@ -4,6 +4,23 @@ namespace modules\restbox\session {
     
 
    class ObjTable extends restbox\AppObject {
+
+        VAR $_CONN_ID;
+
+        function __construct($_req_params,$cfg_info=[],$pmodule=null)
+        {
+            parent::__construct($_req_params,$cfg_info,$pmodule);
+        }
+
+        static function GetRoutePatterns()
+        {
+            return [
+                    'auth/:table:'=>'auth',
+                /*    'tables/one/:table:/:id:'=>'item',
+                    'tables/save/:table:'=>'save',
+                    'tables/delete/:table:'=>'delete',*/
+                ];
+        }
    }
 
 }
