1ff0df91e1d635d39977964acd050e2bc00ef4f5
1datr21 <1datr@mail.ru>
2020-01-27 13:15:16 +0300

    Получили обджект формы

--- modules/restbox.forms/restbox_forms.php
+++ modules/restbox.forms/restbox_forms.php
@@ -2,7 +2,9 @@
 namespace modules\restbox\forms {
 	use Core;
     use Exception;
-    use modules\restbox\RBModule as RBModule;
+	use modules\restbox\RBModule as RBModule;
+	
+	require_once '/inc/obj_form.php';
 
 	class Module extends RBModule 
 	{
@@ -14,12 +16,15 @@ namespace modules\restbox\forms {
 		VAR $_CONF_PATH;
 		VAR $_SETTINGS;
 		VAR $_OBJ_BUF;
+		VAR $_obj_map;
 		
 		function __construct($_PARAMS)
 		{
 			//echo "THIS IS RESTBOX";
-			parent::__construct($_PARAMS);	
-			
+			parent::__construct($_PARAMS);
+			$this->_obj_map	= [
+				'forms'=>'modules\restbox\forms\ObjForm'
+			];
 		}
 
 		function obj_info_by_route($_route)
@@ -48,11 +53,16 @@ namespace modules\restbox\forms {
 			if(!file_exists($form_cfg))
 			{
 				$this->exe_mod_func('restbox','out_error',['message'=>"Form {$f_info['object']['name']} not exists",'errno'=>54]);
+				return;
 			}
-			//print_dbg($form_cfg ) ;
+			$obj_class_name = $this->_obj_map[$f_info['obj_class']];
+			
+			include $form_cfg;
+			$form_obj = new $obj_class_name($info);
+			return $form_obj;
 		}
 
-		function call_obj($_route,$obj_class)   
+		function call_obj($_route,$obj_class=null)   
 		{
 		//	print_dbg($_route);			
 			$obj_nfo = $this->obj_info_by_route($_route);
@@ -62,52 +72,17 @@ namespace modules\restbox\forms {
 			{
 				case 'forms': {
 					$_obj_form = $this->load_form($obj_nfo);
+					return $_obj_form;
 				} break;
 			}
-
-		/*	if($r_pieces[0]=='forms')
-			{
-				$form_name = $r_pieces[1];
-				array_shift($r_pieces);
-				array_shift($r_pieces);
-			//	print_dbg($r_pieces);
-				$_cfg_info = $this->exe_mod_func('restbox', 'get_settings');
-				$form_cfg = url_seg_add($_cfg_info['CFG_DIR'],'forms',$form_name).".php";
-				if(!file_exists($form_cfg))
-				{
-					$this->exe_mod_func('restbox','out_error',['message'=>"Form $form_name not exists",'errno'=>54]);
-				}
-				print_dbg($form_cfg ) ;
-			}
-		*/
-		/*	$ptrn_list = call_user_func($obj_class .'::GetRoutePatterns');
-
-			$_request = call_user_func($obj_class . '::FindPattern', $_route, $ptrn_list);
-			if($_request!=false)
-			{
-				$_o_key = call_user_func($obj_class . '::getKey', $_request['request']);
-				if($this->exe_mod_func('restbox.route','obj_exists',$_o_key))
-				{
-					$_obj = $this->exe_mod_func('restbox.route','get_obj', $_o_key);
-				}
-				else
-				{
-					
-					$_obj = $this->exe_mod_func('restbox.route','add_obj', new $obj_class($_request['request'], $_cfg_info, $this), $_o_key);
-				}
-
-			//	print_dbg($_request);
-				$res_obj = $_obj->ExeAction($_request['action'],$_request['request']);
-				return $res_obj;
-			}
-			*/
+		
 			return null;
 		}
 		
 		function restbox_route_onquery(&$eargs)
 		{	
 		//	print_dbg(":::");			
-			$obj_res = $this->call_obj($eargs['route'],'modules\restbox\forms\ObjForm');
+			$obj_res = $this->call_obj($eargs['route']);
 			
 			return $obj_res;
 		}	
