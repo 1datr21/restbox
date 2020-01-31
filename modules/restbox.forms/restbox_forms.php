5aef2ddca8a46f1188685f9b89cfe425d51c0169
1datr21 <1datr@mail.ru>
2020-01-31 23:33:22 +0300

    --- Auto-Git Commit ---

--- modules/restbox.forms/restbox_forms.php
+++ modules/restbox.forms/restbox_forms.php
@@ -74,7 +74,7 @@ namespace modules\restbox\forms {
 		{
 			$_cfg_info = $this->exe_mod_func('restbox', 'get_settings');
 		//	print_dbg($_cfg_info ) ;
-			$form_cfg = url_seg_add($_cfg_info['CFG_DIR'],'forms',$f_info['object']['name']).".php";
+			$form_cfg = url_seg_add($_cfg_info['CFG_DIR'],$_cfg_info['_EP'],'forms',$f_info['object']['name']).".php";
 			if(!file_exists($form_cfg))
 			{
 				$this->exe_mod_func('restbox','out_error',['message'=>"Form {$f_info['object']['name']} not exists",'errno'=>54]);
