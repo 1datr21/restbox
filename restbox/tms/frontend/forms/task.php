5957a0264afe2f1e320d6f5bdf714fdf26935f66
1datr21 <1datr@mail.ru>
2020-03-23 21:55:51 +0300

    --- Auto-Git Commit ---

--- restbox/tms/frontend/forms/task.php
+++ restbox/tms/frontend/forms/task.php
@@ -31,7 +31,7 @@ $info = new obj_description([
         {
             $_res['name']='Name could not be empty';
         }
-        print_dbg('Validated');
+    //    print_dbg('Validated');
         return $_res;
     }
 ],
