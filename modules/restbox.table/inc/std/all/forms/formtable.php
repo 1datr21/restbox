6104815d6208a2c5f80e09010cf9f0d0fa9a0eb0
1datr21 <1datr@mail.ru>
2020-04-02 23:32:53 +0300

    --- Auto-Git Commit ---

---
+++ modules/restbox.table/inc/std/all/forms/formtable.php
@@ -0,0 +1,29 @@
+<?php
+use modules\restbox as restbox; 
+use modules\restbox\obj_description as obj_description;
+use modules\restbox\table\tfield as tfield;
+use modules\restbox\table;
+
+$info = new obj_description([
+'fields'=>[
+ 
+/*    'id'=>new ffield('hidden'),
+    'name'=>new ffield('text',['maxlen'=>50]),
+    'descr'=>new ffield('textbox',['maxlen'=>2500]),
+    'options'=>new ffield('set',['values'=>['important','attached','group','slow'],'default'=>['slow','attached']]),
+    'active'=>new ffield('bool'),
+    'author'=>new ffield('ref',['table'=>'users']),
+    'createdate'=>new ffield('datetime'),
+    */
+],
+//'table'=>'tasks',
+'events'=>[
+    'OnSubmit'=>function($env,$_p_data) { 
+        return $env->P_MODULE->exe_mod_func('restbox.session','logout');   
+    },
+    'OnValidate'=>function($env,$_p_data) {
+       // print_dbg('Validating form');
+    }
+],
+]);
+?>
