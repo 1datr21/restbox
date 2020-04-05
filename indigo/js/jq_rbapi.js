53978860992356ebb92eba5a9469a630e92bdad3
1datr21 <1datr@mail.ru>
2020-04-05 17:12:13 +0300

    --- Auto-Git Commit ---

--- indigo/js/jq_rbapi.js
+++ indigo/js/jq_rbapi.js
@@ -229,7 +229,7 @@ jq_rbapi.prototype.loadform = function(form_el, fdata) // form info with csrf
         {
             var def = fdata.addinfo[fld].defdata;
 
-        //    this.set_def_val(form_el,fld,def);
+            this.set_def_settings(form_el,fld,def);
         
             if(fdata.addinfo[fld].hasOwnProperty('valuelist'))
             {
@@ -240,7 +240,16 @@ jq_rbapi.prototype.loadform = function(form_el, fdata) // form info with csrf
     }
 }
 
+jq_rbapi.prototype.set_def_settings = function(form_el,fld,fld_val) // set value of element of the form marked fld 
+{
+    var the_element = $(form_el).find('[field='+fld+']');
+    if(the_element!=null) // 
+    {
 
+    }
+    
+    //.val()
+}
 
 jq_rbapi.prototype.action_seg_change = function(theaction,newseg,segno=2) {
  
